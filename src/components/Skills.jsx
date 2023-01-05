1a5a1e57d760b3d5bc5f0d6c2d5e81a2d8de37f9
OmidCode <omidmolaie92290@gmail.com>
2023-01-05 04:10:43 +0100

    about + skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,11 +11,11 @@ import JavaScript from '../assets/javascript.png';
 
 function Skills() {
   return (
-    <div name="skills" className="bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/*container*/}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p className="text-4xl font-bold inline border-b-4 border-[#33FFFC]">Experience</p>
+          <p className="text-4xl font-bold inline border-b-4 border-[#33FFFC]">Skills</p>
           <p className="py-4">// These are the technologies I've worked with </p>
         </div>
 
