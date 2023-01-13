4b0767618c97e37d5b756d34ee26203f133c9a37
OmidCode <omidmolaie92290@gmail.com>
2023-01-13 01:37:41 +0100

    initial commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -42,7 +42,7 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">API for social network with CRUD operations using NodeJS and MongoDB</span>
               <div className="pt-8 text-center">
-                <a href="https://github.com/OmidCode/Omid-Ohmyfood" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/OmidCode/Piiquante-P6" target="_blank" rel="noopener noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
