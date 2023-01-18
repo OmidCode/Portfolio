93fae66dd4a7615d0cfa019fd2ddfdfd52c4e4b8
OmidCode <omidmolaie92290@gmail.com>
2023-01-18 21:56:19 +0100

    Form required champs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,9 +8,9 @@ function contact() {
           <p className="text-4xl font-bold inline border-b-4 border-cyan-300 text-gray-300">Contact</p>
           <p className="text-gray-300 py-4">Submit the form below and let's work togheter</p>
         </div>
-        <input className="bg-[#ccd6f6] p-2" type="text" placeholder="Name" name="name"></input>
-        <input className="my-4 p-2 bg-[#ccd6f6]" type="email" placeholder="Email" name="email"></input>
-        <textarea className="bg-[#ccd6f6] p-2" name="message" rows="10" placeholder="Message"></textarea>
+        <input className="bg-[#ccd6f6] p-2" type="text" placeholder="Name" name="name" required></input>
+        <input className="my-4 p-2 bg-[#ccd6f6]" type="email" placeholder="Email" name="email" required></input>
+        <textarea className="bg-[#ccd6f6] p-2" name="message" rows="10" placeholder="Message" required></textarea>
         <button className="text-white border-2 hover:bg-cyan-300 hover:border-cyan-300 px-4 py-3 my-8 mx-auto flex items-center">Let's Collaborate</button>
       </form>
     </div>
